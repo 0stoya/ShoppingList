3ebbd4c55bb4a2520ad4262380f6761d7a51e228
0stoya <chris@ostoya.co.uk>
2026-05-06 13:26:39 +0100

    Remove admin import feature and fix UI data source exception

--- Controller/Adminhtml/Import/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Ostoya\ShoppingList\Block\Adminhtml\Import;
-
-use Magento\Backend\Block\Template;
-
-class Form extends Template
-{
-    protected $_template = 'Ostoya_ShoppingList::import/form.phtml';
-}
